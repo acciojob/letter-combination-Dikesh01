51cc1f1ceaf7979164c461f51617db8ba82f5be2
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-16 07:35:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+let mapping ={
+	"2" : "abc",
+	"3" : "def",
+	"4" : "ghi",
+	"5" : "jkl",
+	"6" : "mno",
+	"7" : "pqrs",
+	"8" : "tuv",
+	"9" : "wxyz"
 }
+let result = [];
+let searchString = "23"
+function letterCombinations(index, calculatedStr){
+
+	 if(index >= searchString.length){
+		 result.push(calculatedStr);
+	 }
+	let currStr = mapping[searchString[index]];// mapping["2"] => "abc"
+	for(let i=0; i<currStr.length; i++){
+		solve(index+1, calculatedStr + currStr[i])
+	}
+	
+}
+letterCombinations(0, "");
+
+// function letterCombinations(input_digit) {
+//   //Complete the function
+// }
 
 module.exports = letterCombinations;
